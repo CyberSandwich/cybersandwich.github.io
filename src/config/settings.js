ef44ccdcc7988f271429ae35201e588bfb3250f5
CyberSandwich <ventures@saputra.co.uk>
2025-09-23 09:37:26 +0100

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Duke Saputra`,
 	description: `BSc Management @ Warwick Business School | Ex-Microsoft | Ex-IBM`,
-	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
+	url: `https://saputra.co.uk`, // No trailing slash!
 	name: `Duke`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
